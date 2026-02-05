d61a212569203a23d2cdb0a8ba230ddcfc43b440
khanishkar013 <khanishkarchinnusamy@gmail.com>
2026-02-05 16:21:20 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int a=10,b=20;
+    int c=a-b;
+    printf("%d\n",c);
+    int d=a*b;
+    printf("%d\n",d);
+    int e=a/b;
+    printf("%d\n",e);
+    int f=a%b;
+    printf("%d",f);
+    return 0;
+}
