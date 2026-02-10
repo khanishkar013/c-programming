5ba7f7028dc13c2bba4a42a205785e4be17c55a4
khanishkar013 <khanishkarchinnusamy@gmail.com>
2026-02-10 13:21:11 +0530

    Add files via upload

---
+++ voting.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    if (a>=18) {
+        printf("eligible");
+    }
+    else {
+        printf("not eligible");
+    }
+    return 0;
+}
