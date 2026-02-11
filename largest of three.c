b8f3e87b9556e6f9a9bb5a4cbfd7460154e352a0
khanishkar013 <khanishkarchinnusamy@gmail.com>
2026-02-11 11:08:01 +0530

    Add files via upload

--- largest of three.c
+++ largest of three.c
@@ -3,13 +3,13 @@ int main() {
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
     if (a>b && a>c) {
-        printf("a is greater");
+        printf("%d is greater",a);
     }
     else if (b>a && b>c) {
-        printf ("b is greater");
+        printf ("%d is greater",b);
     }
     else {
-        printf ("c is greater");
+        printf ("%d is greater",c);
     }
     return 0;
 }
